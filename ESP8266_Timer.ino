c56f2507b8e75c181b232729080f62b80413fbd1
Ali_Noroozi <alinorozi202020@gmail.com>
2021-07-27 14:02:46 +0430

    complite notes

--- ESP8266_Timer.ino
+++ ESP8266_Timer.ino
@@ -3,7 +3,7 @@
 byte sec = 0, mint = 0, hour = 0;
 unsigned long day = 0;
 
-  void inline interrupt_timer (void){
+  void inline interrupt_timer (void){//timer metod of Timer :: this metod Interrupt Every second
     sec++;
     if(sec == 60){
       sec = 0;
@@ -26,16 +26,16 @@ unsigned long day = 0;
     Serial.println(hour);
     Serial.print("day=");
     Serial.println(day);
-    timer0_write(ESP.getCycleCount()+timer0_preload*2);
+    timer0_write(ESP.getCycleCount()+timer0_preload*2); //set time next Interrupt
     }
 
 void setup() {
   Serial.begin(9600);
-  noInterrupts();
-  timer0_isr_init();
-  timer0_attachInterrupt(interrupt_timer);
-  timer0_write(ESP.getCycleCount()+timer0_preload*2);
-  interrupts();
+  noInterrupts(); 
+  timer0_isr_init();  //begin timer0
+  timer0_attachInterrupt(interrupt_timer); //set metod Interrupt
+  timer0_write(ESP.getCycleCount()+timer0_preload*2); // set time next Interrupt
+  interrupts(); 
 }
 
 void loop() {
